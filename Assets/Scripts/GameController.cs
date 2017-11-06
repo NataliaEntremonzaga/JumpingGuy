1f117a08611a491e1189a6d9e3320df02f2acbf3
Natalia <n.entremonzagamonje@gmail.com>
2017-11-06 10:17:51 +0100

    Juego unity

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum GameState{Idle, Playing, Ended, Ready};
+
+public class GameController : MonoBehaviour {
+
+	[Range (0f, 0.20f)]
+	public float parallaxSpeed = 0.02f;
+	public RawImage background;
+	public RawImage platform;
+
+	public GameObject uiIdle;
+	public GameObject uiScore;
+	public GameObject player;
+
+	public Text pointsText;
+	public Text recordText;
+
+
+	public GameState gameState = GameState.Idle;
+	public GameObject enemyGenerator;
+
+	private AudioSource musicPlayer;
+
+	public float scaleTime = 6f;
+	public float scaleInc = 0.25f;
+
+	private int points =0;
+
+	// Use this for initialization
+	void Start () {
+		musicPlayer = GetComponent<AudioSource> ();
+		recordText.text = "BEST: " + GetMaxScore ().ToString ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (gameState == GameState.Idle && (Input.GetKeyDown ("up") || Input.GetMouseButtonDown (0))) {
+			gameState = GameState.Playing;
+			uiIdle.SetActive (false);
+			uiScore.SetActive (true);
+			player.SendMessage ("UpdateState", "PlayerRun");
+			enemyGenerator.SendMessage ("StartGenerator");
+			musicPlayer.Play ();
+			InvokeRepeating ("GameTimeScale", scaleTime, scaleTime);
+		} 
+		//juego en marcha
+		else if (gameState == GameState.Playing) {
+			Parallax();
+		}
+		//juego para reiniciar
+		else if (gameState == GameState.Ready) {
+			if ( Input.GetKeyDown ("return") || Input.GetMouseButtonDown (0)) {
+				RestartGame ();
+			}
+		}
+	}
+
+	void Parallax(){
+
+		float finalSpeed = parallaxSpeed * Time.deltaTime;	
+		background.uvRect = new Rect (background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+		platform.uvRect = new Rect (platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+
+	}
+
+	public void RestartGame(){
+		ResetTimeScale ();
+		SceneManager.LoadScene ("Level 001");
+	}
+
+	void GameTimeScale(){
+		Time.timeScale += scaleInc;
+		Debug.Log ("ritmo " + Time.timeScale.ToString ());
+	}
+	public void ResetTimeScale(float newTimeScale = 1f){
+		CancelInvoke ("GameTimeScale");
+		Time.timeScale = newTimeScale;
+		Debug.Log ("ritmo restablecido " + Time.timeScale.ToString ());
+	}
+	//Puntuaciones juego
+	public void IncreasePoints(){
+		points++;
+		pointsText.text = points.ToString ();
+		if (points >= GetMaxScore ()) {
+			recordText.text = "BEST: " + points.ToString ();
+			SaveScore (points);
+		}
+	}
+
+	public int GetMaxScore(){
+		return PlayerPrefs.GetInt ("Max Points", 0);
+	}
+
+	public void SaveScore(int currentPoints){
+		PlayerPrefs.SetInt ("Max Points", currentPoints);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Animator animator;
+	public GameObject game;
+	public GameObject enemyGenerator;
+	public AudioClip jumpClip;
+	public AudioClip dieClip;
+	public AudioClip pointClip;
+	private AudioSource audioPlayer;
+	private float startY;
+	public ParticleSystem dust;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		audioPlayer = GetComponent<AudioSource> ();
+		startY = transform.position.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		bool isGrounded = transform.position.y == startY;
+		bool gamePlaying = game.GetComponent<GameController> ().gameState == GameState.Playing;
+		if (isGrounded) {
+			if (gamePlaying) {
+				if (Input.GetKeyDown ("up") || Input.GetMouseButtonDown (0) || Input.GetKeyDown ("space")) {
+					UpdateState ("PlayerJump");
+					//sonido player jump
+					audioPlayer.clip = jumpClip;
+					audioPlayer.Play ();
+				}
+			}
+		}
+	}
+
+	public void UpdateState(string state = null){
+		if(state !=null){
+			animator.Play(state);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == "Enemy") {
+			Debug.Log ("Me Muero¡¡¡");
+			UpdateState ("PlayerDie");
+			game.GetComponent<GameController> ().gameState = GameState.Ended;
+			enemyGenerator.SendMessage ("CancelGenerator", true);
+			game.SendMessage ("ResetTimeScale", 0.5f);
+
+			//sonido player die
+			game.GetComponent<AudioSource> ().Stop ();
+			audioPlayer.clip = dieClip;
+			audioPlayer.Play ();
+			//dust.Stop ();
+		} else if (other.gameObject.tag == "Point") {//Puntuacion del jugador
+			game.SendMessage ("IncreasePoints");
+			//sonido puntuacion
+			audioPlayer.clip = pointClip;
+			audioPlayer.Play ();
+		}
+	}
+	void GameReady(){
+		game.GetComponent<GameController> ().gameState = GameState.Ready;
+	}
+
+	void DustPlay(){
+		dust.Play ();
+	}
+
+	void DustStop(){
+		dust.Stop ();
+	}
+}
